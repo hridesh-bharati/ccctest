f38260feff62f50c83d47013df465198328a2d68
hridesh-bharati <hridesh027@gmail.com>
2025-02-23 12:41:16 +0530

    Fixed Router issue and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import QuizQuestions from "./assets/QuizQuestions";
 import Score from "./assets/Score";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
+import "./App.css";
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<QuizQuestions />} />
-        <Route path="/score/:score/:total" element={<Score />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<QuizQuestions />} />
+      <Route path="/score/:score/:total" element={<Score />} />
+    </Routes>
   );
 };
 
